e681a1c8fbba8f19b6cbdf737d6f01794f349d52
Larry Nicholson <eternalone423@gmail.com>
2012-12-25 22:24:37 -0500

    Initial Push
    Stable

---
+++ Spaz/Flashing/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Spaz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtSpeed = new System.Windows.Forms.TextBox();
+            this.lblSpeed = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.chkRainbow = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(1, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(641, 397);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ಠ_ರೃ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtSpeed
+            // 
+            this.txtSpeed.Location = new System.Drawing.Point(89, 417);
+            this.txtSpeed.Name = "txtSpeed";
+            this.txtSpeed.Size = new System.Drawing.Size(100, 20);
+            this.txtSpeed.TabIndex = 1;
+            this.txtSpeed.Text = "50";
+            // 
+            // lblSpeed
+            // 
+            this.lblSpeed.AutoSize = true;
+            this.lblSpeed.Location = new System.Drawing.Point(12, 420);
+            this.lblSpeed.Name = "lblSpeed";
+            this.lblSpeed.Size = new System.Drawing.Size(71, 13);
+            this.lblSpeed.TabIndex = 2;
+            this.lblSpeed.Text = "Color Speed: ";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(396, 403);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(120, 47);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(522, 403);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(120, 47);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // chkRainbow
+            // 
+            this.chkRainbow.AutoSize = true;
+            this.chkRainbow.Location = new System.Drawing.Point(242, 420);
+            this.chkRainbow.Name = "chkRainbow";
+            this.chkRainbow.Size = new System.Drawing.Size(98, 17);
+            this.chkRainbow.TabIndex = 5;
+            this.chkRainbow.Text = "Rainbow Mode";
+            this.chkRainbow.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 453);
+            this.Controls.Add(this.chkRainbow);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblSpeed);
+            this.Controls.Add(this.txtSpeed);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Spaz";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtSpeed;
+        private System.Windows.Forms.Label lblSpeed;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.CheckBox chkRainbow;
+    }
+}
+
---
+++ Spaz/Flashing/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Spaz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = txtSpeed.Text == string.Empty ? 500 : Convert.ToInt16(txtSpeed.Text);
+            timer1.Enabled = true;
+        }
+
+        public void SwitchColors()
+        {
+            timer1.Interval = txtSpeed.Text == string.Empty ? 500 : Convert.ToInt16(txtSpeed.Text);
+
+            if (chkRainbow.Checked == false)
+            {
+                if (button1.BackColor == Color.Red)
+                    button1.BackColor = Color.Blue;
+                else if (button1.BackColor == Color.Blue)
+                    button1.BackColor = Color.Red;
+                else if (button1.BackColor != Color.Red)
+                     button1.BackColor = Color.Blue;               
+            }
+            else
+            {
+                button1.BackColor = ColorTranslator.FromHtml(GetColor());
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            SwitchColors();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = txtSpeed.Text == string.Empty ? 500 : Convert.ToInt16(txtSpeed.Text);
+            timer1.Enabled = true;
+        }
+
+        private string GetColor()
+        {
+            var random = new Random();
+            var color = String.Format("#{0:X6}", random.Next(0x1000000));
+
+            return color;
+        }
+    }
+}
